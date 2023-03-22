302ab05b85e0b2a7af3f713e05c0f4c222e87eb0
1user1-1 <120933718+1user1-1@users.noreply.github.com>
2023-03-22 16:37:29 +0300

    Created Dependency Injection with setters

--- src/main/java/com/example/demo/Person.java
+++ src/main/java/com/example/demo/Person.java
@@ -5,8 +5,12 @@ import java.sql.SQLOutput;
 public class Person {
     private Pet pet;
 
-    public Person(Pet pet) {
+    public Person() {
         System.out.println("Person bean is created");
+    }
+
+    public void setPet(Pet pet) {
+        System.out.println("Class Person: set Pet");
         this.pet = pet;
     }
 
