0be12c50b8a4cc3e1313f2ce9dd2b303f86aaa86
1user1-1 <120933718+1user1-1@users.noreply.github.com>
2023-04-01 10:48:38 +0300

    Destroy and init method for Spring Container have been created

--- src/main/java/com/example/demo/Dog.java
+++ src/main/java/com/example/demo/Dog.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 public class Dog implements Pet{
+//    private String name;
     @Override
     public void say(){
         System.out.println("Bow-Wow");
@@ -9,4 +10,20 @@ public class Dog implements Pet{
     public Dog() {
         System.out.println("dog bean is created");
     }
+
+    protected void init(){
+        System.out.println("Class Dog: init method");
+    }
+
+    private void destroy(){
+        System.out.println("Class Dog: destroy method");
+    }
+
+//    public String getName(){
+//        return name;
+//    }
+//
+//    public void set Name(String name){
+//        this.name=name;
+//    }
 }
